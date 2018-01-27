ac56438c1e1d676855acfdf85bfcc08d201fa759
IvanGoodmann <xdistil@gmail.com>
2018-01-27 23:54:27 +0300

    work 27.01.18

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
+$(document).ready(function () {
 
+    $('#fullpage').fullpage({
+        sectionSelector: '.vertical-scrolling',
+        slideSelector: '.horizontal-scrolling',
+        navigation: true,
+        slidesNavigation: true,
+        controlArrows: false,
+        touchSensitivity: 10,
+        anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixSection'],
+        menu: '#menu'
+    });
+
+    new WOW().init();
+
+    $(".typed-browser-box").typed({
+        strings: ["lorem", "inpus", "test"],
+        typeSpeed: 200,
+        loop: true
+    });
+
+});
